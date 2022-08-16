dee1142b56515d2b40037d2c0e70f0d4892aee68
Kelvin <kmwendwa476@gmail.com>
2022-08-16 19:30:36 +0300

    commit tree da955467ca90ad369abc46a2342dd095acb07df1

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+#greet
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+greet("name")
+
+#greet_with_default
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+greet_with_default("name")
+
+#add
+def add num1, num2
+  puts num1
+  puts num2
+  num1 + num2
+end
+
+add(2, 4)
+
+#halve
+def halve(num)
+  return nil unless num.class == Integer
+  return num / 2
+end
